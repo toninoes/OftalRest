85bd031450069c872d20c76e479db904f211cca9
Antonio Ruiz <toninoes@gmail.com>
2017-12-22 23:30:54 +0100

    Cambios ligeros en código

--- ServicioWeb/src/pnetSW/Ponente.java
+++ ServicioWeb/src/pnetSW/Ponente.java
@@ -4,69 +4,69 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Ponente {
-	private String dni;
-	private String nombre, apellidos, afiliacion, pais;
+  private String dni;
+  private String nombre, apellidos, afiliacion, pais;
 
-	public Ponente() {}
+  public Ponente() {}
 
-	public Ponente(String dni, String nombre, String apellidos,
-					String afiliacion, String pais) {
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.afiliacion = afiliacion;
-		this.pais = pais;
-	}
+  public Ponente(String dni, String nombre, String apellidos,
+          String afiliacion, String pais) {
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.afiliacion = afiliacion;
+    this.pais = pais;
+  }
 
-	public String getDni() {
-		return dni;
-	}
+  public String getDni() {
+    return dni;
+  }
 
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
 
-	public String getNombre() {
-		return nombre;
-	}
+  public String getNombre() {
+    return nombre;
+  }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-	public String getPais() {
-		return pais;
-	}
+  public String getPais() {
+    return pais;
+  }
 
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
+  public void setPais(String pais) {
+    this.pais = pais;
+  }
 
-	public String getApellidos() {
-		return apellidos;
-	}
+  public String getApellidos() {
+    return apellidos;
+  }
 
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
+  public void setApellidos(String apellidos) {
+    this.apellidos = apellidos;
+  }
 
-	public String getAfiliacion() {
-		return afiliacion;
-	}
+  public String getAfiliacion() {
+    return afiliacion;
+  }
 
-	public void setAfiliacion(String afiliacion) {
-		this.afiliacion = afiliacion;
-	}
+  public void setAfiliacion(String afiliacion) {
+    this.afiliacion = afiliacion;
+  }
 
-	public String toString() {
-		String msg;
+  public String toString() {
+    String msg;
 
-		msg = "DNI: " + this.dni;
-		msg += "\nNOMBRE: " + this.nombre;
-		msg += "\nAPELLIDOS: " + this.apellidos;
-		msg += "\nAFILIACIÓN: " + this.afiliacion;
-		msg += "\nPAÍS: " + this.pais;
+    msg = "DNI: " + this.dni;
+    msg += "\nNOMBRE: " + this.nombre;
+    msg += "\nAPELLIDOS: " + this.apellidos;
+    msg += "\nAFILIACIÓN: " + this.afiliacion;
+    msg += "\nPAÍS: " + this.pais;
 
-		return msg;
-	}
+    return msg;
+  }
 }
--- ServicioWebCliente/WebContent/js/validacion-formulario.js
+++ ServicioWebCliente/WebContent/js/validacion-formulario.js
@@ -1,66 +1,66 @@
 function validarFormulario() {
 
-	patron = /^([a-zA-ZáéíóúAÉÍÓÚÑñ]+)$/i;
-	var nombre = document.getElementById("nombre").value;
-	var apellidos = document.getElementById("apellidos").value;
-	var pago = document.getElementById("pago").selectedIndex;
-	var grupo = document.getElementById("grupo").selectedIndex;
-	var fini = new Date(document.getElementById("llegada").value);
-	var ffin = new Date(document.getElementById("salida").value);
-	var eini = new Date("2017-10-27");
-	var efin = new Date("2017-10-29");
+  patron = /^([a-zA-ZáéíóúAÉÍÓÚÑñ]+)$/i;
+  var nombre = document.getElementById("nombre").value;
+  var apellidos = document.getElementById("apellidos").value;
+  var pago = document.getElementById("pago").selectedIndex;
+  var grupo = document.getElementById("grupo").selectedIndex;
+  var fini = new Date(document.getElementById("llegada").value);
+  var ffin = new Date(document.getElementById("salida").value);
+  var eini = new Date("2017-10-27");
+  var efin = new Date("2017-10-29");
 
-	var respuesta = true;
-	document.getElementById("errornombre").innerHTML = '';
-	document.getElementById("errorapellidos").innerHTML = '';
-	document.getElementById("errorpago").innerHTML = '';
-	document.getElementById("errorgrupo").innerHTML = '';
-	document.getElementById("errorllegada").innerHTML = '';
-	document.getElementById("errorsalida").innerHTML = '';
+  var respuesta = true;
+  document.getElementById("errornombre").innerHTML = '';
+  document.getElementById("errorapellidos").innerHTML = '';
+  document.getElementById("errorpago").innerHTML = '';
+  document.getElementById("errorgrupo").innerHTML = '';
+  document.getElementById("errorllegada").innerHTML = '';
+  document.getElementById("errorsalida").innerHTML = '';
 
-	if (!nombre.match(patron)) {
-		document.getElementById("errornombre").innerHTML = "El NOMBRE sólo puede contener caracteres alfabéticos";
-		respuesta = false;
-	}
+  if (!nombre.match(patron)) {
+    document.getElementById("errornombre").innerHTML = "El NOMBRE sólo puede contener caracteres alfabéticos";
+    respuesta = false;
+  }
 
-	if (!apellidos.match(patron)) {
-		document.getElementById("errorapellidos").innerHTML = "Los APELLIDOS sólo pueden contener caracteres alfabéticos";
-		respuesta = false;
-	}
+  if (!apellidos.match(patron)) {
+    document.getElementById("errorapellidos").innerHTML = "Los APELLIDOS sólo pueden contener caracteres alfabéticos";
+    respuesta = false;
+  }
 
-	if (pago == null || pago == 0) {
-		document.getElementById("errorpago").innerHTML = "Debes seleccionar una forma de PAGO";
-		respuesta = false;
-	}
+  if (pago == null || pago == 0) {
+    document.getElementById("errorpago").innerHTML = "Debes seleccionar una forma de PAGO";
+    respuesta = false;
+  }
 
-	if (grupo == null || grupo == 0) {
-		document.getElementById("errorgrupo").innerHTML = "Debes seleccionar un GRUPO";
-		respuesta = false;
-	}
+  if (grupo == null || grupo == 0) {
+    document.getElementById("errorgrupo").innerHTML = "Debes seleccionar un GRUPO";
+    respuesta = false;
+  }
 
-	if (isNaN(fini.getDate())) {
-		document.getElementById("errorllegada").innerHTML = "Fecha de LLEGADA no es una fecha válida";
-		respuesta = false;
-	}else if (!((fini >= eini) && (fini <= efin))) {
-		document.getElementById("errorllegada").innerHTML = "Fecha de LLEGADA debe estar comprendida entre las fechas del evento";
-		respuesta = false;
-	}
+  if (isNaN(fini.getDate())) {
+    document.getElementById("errorllegada").innerHTML = "Fecha de LLEGADA no es una fecha válida";
+    respuesta = false;
+  }else if (!((fini >= eini) && (fini <= efin))) {
+    document.getElementById("errorllegada").innerHTML = "Fecha de LLEGADA debe estar comprendida entre las fechas del evento";
+    respuesta = false;
+  }
 
-	if (isNaN(ffin.getDate())) {
-		document.getElementById("errorsalida").innerHTML = "Fecha de SALIDA no es una fecha válida";
-		respuesta = false;
-	} else if (!((ffin >= eini) && (ffin <= efin))) {
-		document.getElementById("errorsalida").innerHTML = "Fecha de SALIDA debe estar comprendida entre las fechas del evento";
-		respuesta = false;
-	}
+  if (isNaN(ffin.getDate())) {
+    document.getElementById("errorsalida").innerHTML = "Fecha de SALIDA no es una fecha válida";
+    respuesta = false;
+  } else if (!((ffin >= eini) && (ffin <= efin))) {
+    document.getElementById("errorsalida").innerHTML = "Fecha de SALIDA debe estar comprendida entre las fechas del evento";
+    respuesta = false;
+  }
 
-	if (!isNaN(fini.getDate()) && !isNaN(ffin.getDate()) && fini > ffin) {
-		document.getElementById("errorllegada").innerHTML = "La Fecha de LLEGADA es posterior a la de SALIDA";
-		respuesta = false;
-	}
+  if (!isNaN(fini.getDate()) && !isNaN(ffin.getDate()) && fini > ffin) {
+    document.getElementById("errorllegada").innerHTML = "La Fecha de LLEGADA es posterior a la de SALIDA";
+    respuesta = false;
+  }
 
-	if(respuesta)
-		alert("VALIDACION CORRECTA");
+  if(respuesta)
+    alert("VALIDACION CORRECTA");
 
-	return respuesta;
+  return respuesta;
 }
